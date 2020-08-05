d478bdd862257544b64db1ab068ae7ae2d36a085
ofk8vb <latios_gs@hotmail.com>
2020-08-05 16:51:34 +0300

    fourth commit

---
+++ FinalProject/face-recognizer/smart-brain-api/server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const database = {
+	users:[
+	{
+		id: '123',
+		name: 'John',
+		email: 'john@gmail.com',
+		entries:0,
+		joined:new Date()
+	},
+	{
+		id: '124',
+		name: 'Sally',
+		email: 'Sally@gmail.com',
+		entries:0,
+		joined:new Date()
+	}]
+}
+
+app.get('/',(req,res)=>{
+	res.send(database.users)
+})
+
+
+app.post('/signin',(req,res)=>{
+	if(req.body.email === database.users[0].email&&
+		req.body.password=== database.users[0].password){
+		res.json('success')
+	}else{
+		res.status(400).json('error logging in');
+	}
+	res.json('signing in')
+})
+
+app.post('/register',(req,res)=>{
+	const {email,name,password} = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password:password,
+		entries:0,
+		joined:new Date()
+
+	})
+	res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id',(req,res)=>{
+	const{id} = req.params;
+	let found = false;
+	database.users.forEach(user =>{
+		if(user.id === id){
+			found=true;
+			return res.json(user);
+		}
+		
+	})
+
+	if(!found){
+		res.status(400).json('not found')
+	}
+
+
+
+})
+
+app.put('/image',(req,res)=>{
+	const{id} = req.body;
+	let found = false;
+	database.users.forEach(user=>{
+		if(user.id===id){
+			found=true;
+			user.entries++
+			return res.json(user.entries);
+		}
+	})
+	if(!found){
+		res.status(400).json('not found');
+	}
+})
+
+
+app.listen(3000,()=>{
+	console.log('app is running on port 3000')
+});
+
+/*
+/we have set our endpoints, the http method we will use , and what will be the response to these requests
+
+/-->res = this is working
+/signin route --> POST request ---> will respond with success/fail
+/register ---> POST = new user object will be returned to server for registration
+/profile/:userId ----> GET = user
+/image ----> PUT ----> updated user object
+
+
+*/

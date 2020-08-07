28cd82b865de0ce53386ad6b5246c83a0f3442c0
ofk8vb <latios_gs@hotmail.com>
2020-08-07 17:55:17 +0300

    almost there

--- FinalProject/face-recognizer/smart-brain-api/server.js
+++ FinalProject/face-recognizer/smart-brain-api/server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const knex = require('knex')
+const bcrypt = require('bcrypt-nodejs')
+
+const db = knex({
+	client:'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password:'test',
+		database: 'smart-brain'
+	}
+
+});
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
+
+
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
@@ -50,31 +70,48 @@ app.post('/signin',(req,res)=>{
 
 app.post('/register',(req,res)=>{
 	const {email,name,password} = req.body;
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries:0,
-		joined:new Date()
-
-	})
-	res.json(database.users[database.users.length-1])
+	const hash= bcrypt.hashSync(password);
+		db.transaction(trx=>{
+			trx.insert({
+				hash:hash,
+				email:email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail=>{
+				return db('users')
+					.returning('*')
+					.insert({
+					email:loginEmail[0],
+					name:name,
+					joined:new Date()
+				})
+				.then(user=>{
+					res.json(user[0]);
+				})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		})	
+		.catch(err=>res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id',(req,res)=>{
 	const{id} = req.params;
-	let found = false;
-	database.users.forEach(user =>{
-		if(user.id === id){
-			found=true;
-			return res.json(user);
+	db.select('*').from('users').where({
+		id: id
+	}).then(user=>{
+		console.log(user)
+		if(user.length){
+			res.json(user[0]);
+		}else{
+			res.status(400).json('error getting user')
 		}
 		
-	})
-
-	if(!found){
-		res.status(400).json('not found')
-	}
+	}).catch(err => res.status(400).json('error getting user'))
+	// if(!found){
+	// 	res.status(400).json('not found')
+	// }
 
 
 
@@ -82,17 +119,16 @@ app.get('/profile/:id',(req,res)=>{
 
 app.put('/image',(req,res)=>{
 	const{id} = req.body;
-	let found = false;
-	database.users.forEach(user=>{
-		if(user.id===id){
-			found=true;
-			user.entries++
-			return res.json(user.entries);
-		}
+	
+	db('users').where('id','=',id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries=>{
+		res.json(entries[0]);
+
 	})
-	if(!found){
-		res.status(400).json('not found');
-	}
+	.catch(err => res.status(400).json('unable to get entries'))
+	
 })
 
 
